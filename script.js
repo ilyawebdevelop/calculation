cba36c308121b5aeea66b31fcf799469c736396e
ilyawebdevelop <ilia966@tut.by>
2022-03-18 11:38:19 +0300

    правки по структуре

--- script.js
+++ script.js
@@ -9,49 +9,37 @@ let cashbackSumActive4 = document.getElementById('sale-sum_4');
 // Удаление у всех элементов "Visibility"
 function removeVisibility() {
   saleAllEl.forEach((el) => {
-    el.style.visibility = 'hidden';    
+    el.style.visibility = 'hidden';
   })
 }
 // Удаление у всех элементов зеленого цвета
 function removeColor() {
   cashbackAllEl.forEach((el) => {
-    el.style.color = '#d2d2d3';    
+    el.style.color = '#d2d2d3';
   })
 }
 
-saleAllEl.forEach((el) => {
-  el.style.visibility = 'hidden';    
-})
-
-
-
-
-
 // Поведение при выборе количества покупок
-range.onchange = function() {	
+range.onchange = function () {
   removeVisibility();
   removeColor();
-  let rangeValue = range.value;
-  console.log(rangeValue);
-
+  let rangeValue = range.value; 
   if (rangeValue <= 4) {
     totalSum.innerHTML = 2000 + ' ₽';
-    cashbackSumActive2.style.color = '#8dbd57';   
-  } 
+    cashbackSumActive2.style.color = '#8dbd57';
+  }
   if (rangeValue <= 1) {
     totalSum.innerHTML = 0 + ' ₽';
     removeColor();
-  } 
+  }
   if (rangeValue >= 5) {
     totalSum.innerHTML = 4000 + ' ₽';
     cashbackSumActive4.style.color = '#8dbd57';
-  } 
-  
-
+  }
   let saleId = 'sale' + rangeValue;
-  let saleIdEl = document.getElementById(saleId);    
-  saleIdEl.style.visibility = 'visible';  
- }
+  let saleIdEl = document.getElementById(saleId);
+  saleIdEl.style.visibility = 'visible';
+}
 
 
 
@@ -67,7 +55,7 @@ range.onchange = function() {
 // let result = document.getElementById('calculation__sum-change');
 // let resultValue = range.value;
 // let calcMonthSum = document.getElementById('calculation__month-sum');
-// range.onchange = function() {	
+// range.onchange = function() {
 // 	let resultSum = range.value * 0.04;
 // 	result.innerHTML = range.value;
 // 	calcMonthSum.innerHTML = resultSum + '<span> ₽</span>';
